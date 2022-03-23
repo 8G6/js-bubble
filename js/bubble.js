99d42930080409434f2ee0c810c4eca1595d79d7
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-23 20:55:42 +0530

    auto-update-bubble.js(20:55:38)

--- js/bubble.js
+++ js/bubble.js
@@ -87,7 +87,7 @@ class Bubble{
             $(`#bubble-${i}`).style.boxShadow = `inset ${0.5*this.sizes[i]*this.y[i]/this.max_x}px 
                                                       ${0.5*this.sizes[i]*this.x[i]/this.max_x}px 
                                                       ${0.5*this.sizes[i]*this.x[i]/this.max_y}px 
-                                                      ${0.5*this.sizes[i]*this.y[i]/this.max_x}px white`
+                                                      ${0.5*this.sizes[i]*this.y[i]/this.max_x}px ${ra}`
            
         }
         console.log(this.x,this.y)
