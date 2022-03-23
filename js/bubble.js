6ef68f87843547cd2ef28fae869921a3f97b962e
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-23 19:23:25 +0530

    auto-update-bubble.js(19:23:22)

--- js/bubble.js
+++ js/bubble.js
@@ -53,7 +53,7 @@ class bubble{
         for(i=0;i<this.bubble_count;i++){
             div=document.createElement('div');
             div.className = 'bubble'
-            div.style.
+            div.style.height = 
         }
     }
 }
