1c6d9491f6e5c2c89d7a1f631ddbee0851ceb3f9
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-23 19:59:52 +0530

    auto-update-main.js(19:59:48)

--- js/main.js
+++ js/main.js
@@ -9,3 +9,14 @@ else{
         this.x[i]+=this.randSpeed ? rand(this.randSpeeds.x): this.x_speed
     }
 }
+if(this.x[i]>=0 && this.x[i]<=this.max_x-0 && this.bool_x[i]) this.x[i]+=this.randSpeed ? rand(this.randSpeeds.y): this.x_speed
+else{
+    if(this.x[i]>=0){
+        this.bool_x[i] = 0
+        this.x[i]-=this.randSpeed ? rand(this.randSpeeds.x): this.x_speed
+    }
+    else{
+        this.bool_x[i] = 1
+        this.x[i]+=this.randSpeed ? rand(this.randSpeeds.x): this.x_speed
+    }
+}
