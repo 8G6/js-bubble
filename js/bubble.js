4d5b909f663a371bde71accc92e2111b69135879
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-23 19:31:57 +0530

    auto-update-bubble.js(19:31:56)

--- js/bubble.js
+++ js/bubble.js
@@ -48,6 +48,7 @@ class bubble{
         return [x,y]
     }
     animate = () =>{
+        $('.main').classList.toggle('bg')
         let i=0,div,y=0;
         for(i=0;i<this.bubble_count;i++){
             y=rand(this.randSizes)
