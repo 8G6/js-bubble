81f9f1653f3e79d0a8e01f37d8feab44147a6af7
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-23 19:59:37 +0530

    auto-update-main.js(19:59:34)

--- js/main.js
+++ js/main.js
@@ -1,60 +1,22 @@
-
-[x,y] = [121,121]
-document.getElementById('background').style.display = 'none'
-let {atan,tan,PI} = Math 
-function angle(x,y){
-    return x-(2*y)
-}
-f=0
-function range(start,end){
-    let array=[]
-    for(i=start;i<end;i++){
-        array.push(i)
-    }
-    return array
-}
-
-color = toHex(0,256)
-
-let disp=document.querySelector('.loading-bg')
-let max_x = disp.scrollWidth
-let max_y = disp.scrollHeight
-let [bool_x,bool_y] = [1,1]
-
-function get(x,y,x_k,y_k){
-
-    if(x>=120 && x<=max_x-121 && bool_x) x+=x_k
-    else{
-        if(x>=121){
-            bool_x = 0
-            x-=x_k
-        }
+if(this.x[i]>=0 && this.x[i]<=this.max_x-0 && this.bool_x[i]) this.x[i]+=this.randSpeed ? rand(this.randSpeeds.y): this.x_speed
         else{
-            bool_x = 1
-            x+=x_k
+            if(this.x[i]>=0){
+                this.bool_x[i] = 0
+                this.x[i]-=this.randSpeed ? rand(this.randSpeeds.x): this.x_speed
+            }
+            else{
+                this.bool_x[i] = 1
+                this.x[i]+=this.randSpeed ? rand(this.randSpeeds.x): this.x_speed
+            }
         }
-    }
-    if(y>=120 && y<=max_y-121  && bool_y) y+=y_k
-    else{
-        if(y>=120){
-            bool_y = 0
-            y-=y_k
-        }
+        if(this.y[i]>=0 && this.y[i]<=this.max_y-0  && this.bool_y[i]) this.y[i]+=this.randSpeed ? rand(this.randSpeeds.y): this.y_speed
         else{
-            bool_y = 1
-            y+=y_k
+            if(this.y[i]>=0){
+                this.bool_y[i] = 0
+                this.y[i] -= this.randSpeed ? rand(this.randSpeeds.y): this.y_speed
+            }
+            else{
+                this.bool_y[i] = 1
+                this.y[i] += this.randSpeed ? rand(this.randSpeeds.y): this.y_speed
+            }
         }
-    }
-    return [x,y]
-}
-let t='black'
-let k = setInterval(()=>{
-    [x,y]=get(x,y,2,5)
-    if(x%10==0){
-        t=randomColor()
-    }
-    document.querySelector('#dev').style.top = `${y}px`
-    document.querySelector('#dev').style.left = `${x}px`
-    document.querySelector('#dev').style.boxShadow = `inset ${y/100}px ${x/100}px ${y/100}px ${x/100}px white`
-    console.log(`inset ${x%5}px ${y%10}px 10px white;`)
-},10)
