d70fbc68f3a215f6ba09499e688889fd90ba1f8a
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-23 20:12:39 +0530

    auto-update-bubble.js(20:12:35)

--- js/bubble.js
+++ js/bubble.js
@@ -57,7 +57,7 @@ class bubble{
             y=rand(this.randSizes)
             div=document.createElement('div');
             div.className = 'bubble'
-            div.id        = ``
+            div.id        = `bubble-${i}`
             div.style.width  = `${y}px`
             div.style.height = `${y}px`
             this.parent.appendChild(div)
