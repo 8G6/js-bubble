8d30dea8a5e6a7f51fe26ed8a595b5a88da4cf93
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-23 19:44:09 +0530

    auto-update-bubble.js(19:44:08)

--- js/bubble.js
+++ js/bubble.js
@@ -61,16 +61,7 @@ class bubble{
             div.style.height = `${y}px`
             this.parent.appendChild(div)
         }
-        for(i=0;i<this.bubble_count;i++){
-            this.x[i]=0
-            this.y[i]=0;
-            for(let j=0;j<this.bubble_count;j++){
-                [x[i],y[i]]=this.path(x,y,j)
-                console.log([x[i],y[i]])
-                div.style.left = `${x[i]}px`
-                div.style.top  = `${y[i]}px`
-            }
-        }
+        
     }
 }
 
