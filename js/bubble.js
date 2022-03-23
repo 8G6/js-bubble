ccb2267a6aa2e7f4262ed2a1531175b4b5b8f9b5
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-23 19:52:05 +0530

    auto-update-bubble.js(19:52:05)

--- js/bubble.js
+++ js/bubble.js
@@ -65,7 +65,7 @@ class bubble{
         let i;
         for(i=0;i<this.bubble_count;i++){
             [this.x[i],this.y[i]] = this.path(this.x[i],this.y[i],i)
-            console.log(this.[i],y[i])
+            console.log(this.x[i],this.y[i])
         }
     }
     start = (timeout) =>{
