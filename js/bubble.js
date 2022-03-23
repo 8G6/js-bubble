b946a8e8fe9232c07325510ed86ab5a0c0f8befe
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-23 18:58:16 +0530

    auto-update-bubble.js(18:58:10)

--- js/bubble.js
+++ js/bubble.js
@@ -1,7 +1,25 @@
 let $ = (a) => document.querySelector(a)
-
+let rand  = (a) => a[Math.floor(Math.random()*a.length)]
+let range = (start,end,step=1) => {
+    let arr = []
+    let i;
+    for(i=start;i<end+1;i+=step)
+        arr.push(i)
+    return arr
+}
 class bubble{
-    constructor(parent_id){
-        this.parent = $('#'+parent_id)
+    constructor(parent_id,bubble_count,x_speed=1,y_speed=1){
+        this.parent       = $('#'+parent_id)
+        this.ch
+        this.bubble_count = bubble_count
+        this.bool_x       = Array(this.bubble_count).fill(1)
+        this.bool_y       = Array(this.bubble_count).fill(1)
+        this.max_x        = document.body.scrollWidth
+        this.max_y        = document.body.scrollHeight
+        this.members_x    = range(0,this.max_x,x_speed)
+        this.members_y    = range(0,this.max_y,y_speed)
+
+        
+
     }
 }
