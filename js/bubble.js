823f375a067260ad1f64012313c19bcd59a80ed1
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-23 21:25:42 +0530

    auto-update-bubble.js(21:25:41)

--- js/bubble.js
+++ js/bubble.js
@@ -48,7 +48,7 @@ class Bubble{
         this.colorSatIndex  = 25
         this.color          = '#ffffff'
         this.randColor      = false
-        this.randColor
+        this.randColorSp
     }
     physics = (i,randSpeeds)=>{
         if(this.x[i]>=0 && this.x[i]<=this.max_x-this.x_offset && this.bool_x[i]) this.x[i]+=this.randSpeed ? rand(this.randSpeeds.y): this.x_speed
