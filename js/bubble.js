ac2762a35ea9c652fe4c2cf7fb74f6b3d4fb0c95
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-23 20:15:34 +0530

    auto-update-bubble.js(20:15:34)

--- js/bubble.js
+++ js/bubble.js
@@ -63,7 +63,7 @@ class bubble{
             div.style.width  = `${y}px`
             div.style.height = `${y}px`
             this.parent.appendChild(div)
-            this.x[i] = rand(range(0,this.max_x))
+            this.x[i] = rand(this.)
             this.y[i] = rand(range(0,this.max_y))
         }
     }
