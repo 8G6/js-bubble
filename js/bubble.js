e5d4627bd87398d333fafa3bf117462f12aeb7ad
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-23 20:11:59 +0530

    auto-update-bubble.js(20:11:54)

--- js/bubble.js
+++ js/bubble.js
@@ -64,6 +64,7 @@ class bubble{
     }
     animate = () =>{
         let i;
+        
         for(i=0;i<this.bubble_count;i++){
             [this.x[i],this.y[i]] = this.path(this.x[i],this.y[i],i)
         }
