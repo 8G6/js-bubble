cdcba234ec203519585eb461734e5cfc0609625d
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-23 19:15:24 +0530

    auto-update-bubble.js(19:15:22)

--- js/bubble.js
+++ js/bubble.js
@@ -19,7 +19,7 @@ class bubble{
         this.x_speed      = x_speed
         this.y_speed      = y_speed
         this.randSpeed    = range(1,15)
-        this.ra
+        this.random
     }
     path = (x,y,i)=>{
         if(this.x[i]>=0 && this.x[i]<=this.max_x-0 && bool_x) this.x[i]+=x_k
