25276cee393ae7e1bbf3fc02407d93b8ae7d0293
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-23 20:24:11 +0530

    auto-update-bubble.js(20:24:06)

--- js/bubble.js
+++ js/bubble.js
@@ -13,7 +13,7 @@ class bubble{
         this.bubble_count = bubble_count
         this.bool_x       = Array(this.bubble_count).fill(1)
         this.bool_y       = Array(this.bubble_count).fill(1)
-        this.max_x        = screen.width-100
+        this.max_x        = screen.width
         this.max_y        = screen.height
         this.x_speed      = x_speed
         this.y_speed      = y_speed
