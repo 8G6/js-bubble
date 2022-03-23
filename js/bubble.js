b3fdd100f31c5faaf2d4f46867fd0dc6a1d49fd2
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-23 19:55:46 +0530

    auto-update-bubble.js(19:55:45)

--- js/bubble.js
+++ js/bubble.js
@@ -39,11 +39,11 @@ class bubble{
         if(this.y[i]>=0 && this.y[i]<=this.max_y-0  && this.bool_y[i]) this.y[i]+=this.randSpeed ? rand(this.randSpeeds.y): this.y_speed
         else{
             if(y>=0){
-                this.bool_y[i] = 0
+                this.bool_y[i] = 1
                 this.y[i] -= this.randSpeed ? rand(this.randSpeeds.y): this.x_speed
             }
             else{
-                this.bool_y[i] = 1
+                this.bool_y[i] = 0
                 this.y[i] += this.randSpeed ? rand(this.randSpeeds.y): this.x_speed
             }
         }
