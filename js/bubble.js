c05fb6bff6d2e523fb449538085b4fbee482427c
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-23 21:12:55 +0530

    auto-update-bubble.js(21:12:53)

--- js/bubble.js
+++ js/bubble.js
@@ -33,7 +33,7 @@ class Bubble{
         this.randSpeeds     = {x:range(1,7),y:range(1,7)}
         this.randSpeed      = false
         this.randSize       = false
-        this.randSizeLim    = {}
+        this.randSizeLim    = {start:10,end:50}
         this.randSizes      = range(60,350,6)
         this.x              = Array(this.bubble_count).fill(0)
         this.y              = Array(this.bubble_count).fill(0)
