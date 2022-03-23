ad5a48b7b874360916b8c66d5d420c30f3663e61
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-23 21:30:03 +0530

    auto-update-bubble.js(21:29:59)

--- js/bubble.js
+++ js/bubble.js
@@ -117,7 +117,7 @@ class Bubble{
 
     }
     start = (timeout=this.timeout) =>{
-        this.randColorSpeed * 1/((this.max_x+this.max_y)/2)
+        this.randColorSpeed * (1/((this.max_x+this.max_y)/2))
         this.init()
         setInterval(this.animate,timeout)
     }
