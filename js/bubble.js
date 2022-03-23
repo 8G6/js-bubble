fc6327ff33b9b51c734c1765595389127cc20f0b
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-23 20:30:12 +0530

    auto-update-bubble.js(20:30:11)

--- js/bubble.js
+++ js/bubble.js
@@ -29,7 +29,7 @@ class bubble{
         this.y_offset     = 2
         this.x_offset     = 100
     }
-    path = (x,y,i)=>{
+    path = (i)=>{
         if(this.x[i]>=0 && this.x[i]<=this.max_x-this.x_offset && this.bool_x[i]) this.x[i]+=this.randSpeed ? rand(this.randSpeeds.y): this.x_speed
         else{
             if(this.x[i]>=this.x_offset){
