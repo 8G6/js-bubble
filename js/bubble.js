e6df61ef6d99ddd5d89e84acab52ed2195e9401d
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-23 21:02:53 +0530

    auto-update-bubble.js(21:02:51)

--- js/bubble.js
+++ js/bubble.js
@@ -44,9 +44,9 @@ class Bubble{
         this.timeout        = 15
         this.sizes          = []
         this.c              = 0
-        this.colorSatIndex = 
-        this.color        = '#ffffff'
-        this.randomColor  = false
+        this.colorSatIndex  = 
+        this.color          = '#ffffff'
+        this.randomColor    = false
     }
     path = (i,randSpeeds)=>{
         if(this.x[i]>=0 && this.x[i]<=this.max_x-this.x_offset && this.bool_x[i]) this.x[i]+=this.randSpeed ? rand(this.randSpeeds.y): this.x_speed
