a2d2119c3beb4820b0bae642b54c31d4d22c012b
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-23 20:00:57 +0530

    auto-update-bubble.js(20:00:56)

--- js/bubble.js
+++ js/bubble.js
@@ -26,26 +26,26 @@ class bubble{
     }
     path = (x,y,i)=>{
         if(this.x[i]>=0 && this.x[i]<=this.max_x-0 && this.bool_x[i]) this.x[i]+=this.randSpeed ? rand(this.randSpeeds.y): this.x_speed
-else{
-    if(this.x[i]>=0){
-        this.bool_x[i] = 0
-        this.x[i]-=this.randSpeed ? rand(this.randSpeeds.x): this.x_speed
-    }
-    else{
-        this.bool_x[i] = 1
-        this.x[i]+=this.randSpeed ? rand(this.randSpeeds.x): this.x_speed
-    }
-}
-if(this.y[i]>=0 && this.y[i]<=this.max_y-0 && this.bool_y[i]) this.y[i]+=this.randSpeed ? rand(this.randSpeeds.y): this.y_speed
-else{
-    if(this.y[i]>=0){
-        this.bool_y[i] = 0
-        this.y[i]-=this.randSpeed ? rand(this.randSpeeds.y): this.y_speed
-    }
-    else{
-        this.bool_y[i] = 1
-        this.y[i]+=this.randSpeed ? rand(this.randSpeeds.y): this.y_speed
-    }
+        else{
+            if(this.x[i]>=0){
+                this.bool_x[i] = 0
+                this.x[i]-=this.randSpeed ? rand(this.randSpeeds.x): this.x_speed
+            }
+            else{
+                this.bool_x[i] = 1
+                this.x[i]+=this.randSpeed ? rand(this.randSpeeds.x): this.x_speed
+            }
+        }
+        if(this.y[i]>=0 && this.y[i]<=this.max_y-0 && this.bool_y[i]) this.y[i]+=this.randSpeed ? rand(this.randSpeeds.y): this.y_speed
+        else{
+            if(this.y[i]>=0){
+                this.bool_y[i] = 0
+                this.y[i]-=this.randSpeed ? rand(this.randSpeeds.y): this.y_speed
+            }
+            else{
+                this.bool_y[i] = 1
+                this.y[i]+=this.randSpeed ? rand(this.randSpeeds.y): this.y_speed
+            }
 }
 
         return [this.x[i],this.y[i]]
